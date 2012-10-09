2f616b5b3cbefb5e44d48140bcf70d0cc67db70b
Ernesto Hernández <ehdez@extrema-sistemas.com>
2012-10-09 10:29:31 +0200

    added samples.di.example4 (circular dependencies)

---
+++ src/main/java/samples/di/example4/BeanA.java
@@ -0,0 +1,20 @@
+package samples.di.example4;
+
+public class BeanA {
+
+	public BeanA(){
+		System.out.println("BeanA created");
+	}
+	private BeanB beanB;
+	
+	public void setBeanB(BeanB beanB) {
+		this.beanB = beanB;
+		System.out.println("setting BeanB");
+		
+	}
+
+	public BeanB getBeanB() {
+		return beanB;
+	}
+	
+}
